b9249f11a70a231186d63eb39260c0466576bf3b
Ivan Raštegorac <ivan.rastegorac@gmail.com>
2022-04-12 19:41:20 +0200

    Add Search Filter

--- pages/api/search.js
+++ pages/api/search.js
@@ -4,8 +4,14 @@ import Image from 'next/image';
 import { Flex, Box, Text, Icon } from '@chakra-ui/react';
 import { BsFilter } from 'react-icons/bs';
 
-const Search = () => {
-  const [searchFilters, setSearchFilter] = useState(false);
+import Property from '../components/Property';
+import SearchFilters from '../components/SearchFilters';
+import { baseUrl, fetchApi } from '../utils/fetchApi';
+import noresult from '../assets/images/noresult.svg';
+import { fetchApi } from '../../utils/fetchApi';
+
+const Search = ({ properties }) => {
+  const [searchFilters, setSearchFilters] = useState(false);
   const router = useRouter();
 
   return (
@@ -51,5 +57,28 @@ const Search = () => {
     </Box>
   );
 };
+
+export async function getServerSideProps({ query }) {
+  const purpose = query.purpose || 'for-rent';
+  const rentFrequency = query.rentFrequency || 'yearly';
+  const minPrice = query.minPrice || '0';
+  const maxPrice = query.maxPrice || '1000000';
+  const roomsMin = query.roomsMin || '0';
+  const bathsMin = query.bathsMin || '0';
+  const sort = query.sort || 'price-desc';
+  const areaMax = query.areaMax || '35000';
+  const locationExternalIDs = query.locationExternalIDs || '5002';
+  const categoryExternalID = query.categoryExternalID || '4';
+
+  const data = await fetchApi(
+    `${baseUrl}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`
+  );
+
+  return {
+    props: {
+      properties: data?.hits,
+    },
+  };
+}
 
 export default Search;
